b78b48dae3804423d69ed56812c14d9808f1ec74
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-09-13 14:42:03 -0500

    chore: Branch created and initial test class correpsonding to 7.1 done

---
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -0,0 +1,23 @@
+package edu.eci.dosw.tdd.skyrescue.center;
+
+//import edu.eci.dosw.tdd.skyrescue.drone.Drone;
+//import edu.eci.dosw.tdd.skyrescue.mission.Mission;
+//import edu.eci.dosw.tdd.skyrescue.mission.MissionStatus;
+//import edu.eci.dosw.tdd.skyrescue.operator.RescueOperator;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RescueCenterTest {
+    private RescueCenter rescueCenter;
+
+    @BeforeEach
+    void setUp() {
+        rescueCenter = new RescueCenter();
+    }
+
+}
