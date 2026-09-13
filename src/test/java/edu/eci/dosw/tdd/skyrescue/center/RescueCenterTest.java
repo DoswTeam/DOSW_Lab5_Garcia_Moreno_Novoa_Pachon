5d51670505960ac43be511f6d69d0cc9ffc42eaa
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-09-13 15:01:25 -0500

    feat: assign mission test done

--- src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -1,10 +1,16 @@
 package edu.eci.dosw.tdd.skyrescue.center;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import edu.eci.dosw.tdd.skyrescue.drone.Drone;
+import edu.eci.dosw.tdd.skyrescue.mission.Mission;
+import edu.eci.dosw.tdd.skyrescue.mission.MissionStatus;
+import edu.eci.dosw.tdd.skyrescue.operator.RescueOperator;
 
 
 class RescueCenterTest {
@@ -20,6 +26,21 @@ class RescueCenterTest {
         Drone drone = new Drone("D1", "DJI Mini 4K", 15);
         boolean registered = rescueCenter.addDrone(drone);
         assertTrue(registered);
+    }
+
+    @Test
+    void shouldAssignMissionWhenOperatorAndDroneAreValidAndDistanceIsAllowed() {
+
+        RescueOperator operator = new RescueOperator("O1", "Ana Torres");
+        Drone drone = new Drone("D1", "DJI Mini 4K", 15);
+        rescueCenter.addOperator(operator);
+        rescueCenter.addDrone(drone);
+
+        Mission mission = rescueCenter.assignMission("O1", "D1", "Calle 10 con Carrera 5", 10);
+
+        assertNotNull(mission);
+        assertEquals(MissionStatus.ACTIVE, mission.getStatus());
+        assertFalse(drone.isAvailable());
     }
 
 }
