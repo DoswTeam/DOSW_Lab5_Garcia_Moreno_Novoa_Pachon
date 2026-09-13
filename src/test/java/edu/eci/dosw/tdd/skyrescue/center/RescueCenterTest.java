41d11aebdcd44068b7c967f9784f7d47f329bbab
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-09-13 18:45:25 -0500

    feat: GREEN - validates that the drone id is not empty

--- src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
+++ src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -98,6 +101,108 @@ class RescueCenterTest {
         // Assert
         assertFalse(rescueCenter.addDrone(drone));
     }
+
+    @Test
+    void shouldNotAssignMissionToNotAvailableDrone(){
+        // Arrange
+        String droneId = "D01";
+        String droneModel = "DJI Mini 5";
+        int droneMaxRangeKm = 200;
+
+        String operatorId = "O01";
+        String operatorName = "Pedro Perez";
+
+        String missionLocation = "Bogotá";
+        int missionDistanceKm = 100;
+
+        // Act
+        Drone drone = new Drone(droneId, droneModel, droneMaxRangeKm);
+        RescueOperator rOperator = new RescueOperator(operatorId, operatorName);
+
+        rescueCenter.addOperator(rOperator);
+        rescueCenter.addDrone(drone);
+        drone.setAvailable(false);
+
+        //Assert
+        assertThrows(IllegalStateException.class, () -> rescueCenter.assignMission(operatorId, 
+            droneId, missionLocation, missionDistanceKm));
+        
+    }
+
+    @Test 
+    void shouldNotCompleteMissionTwice(){
+        //Arrange
+        String droneId = "D01";
+        String droneModel = "DJI Mini 5";
+        int droneMaxRangeKm = 200;
+
+        String operatorId = "O01";
+        String operatorName = "Pedro Perez";
+
+        String missionId = "M1";
+        String missionLocation = "Bogotá";
+        int missionDistanceKm = 100;
+        LocalDateTime missionStartDate = LocalDateTime.now();
+        MissionStatus missionStatus = MissionStatus.ACTIVE;
+
+        //Act
+        Drone drone = new Drone(droneId, droneModel, droneMaxRangeKm);
+        RescueOperator rOperator = new RescueOperator(operatorId, operatorName);
+        Mission mission = new Mission(missionId, missionLocation, missionDistanceKm, 
+            drone, rOperator, missionStartDate, missionStatus);
+
+        rescueCenter.addDrone(drone);
+        rescueCenter.addOperator(rOperator);
+        rescueCenter.assignMission(operatorId, droneId, missionLocation, missionDistanceKm);
+        rescueCenter.completeMission(missionId);
+
+        //Assert
+        assertThrows(IllegalStateException.class, () -> rescueCenter.completeMission(missionId));
+    }
+
+    @Test
+    void shouldNotRegisterTwoDronesWithSameId(){
+        Drone drone1 = new Drone("D5", "Pro-Max", 25);
+        Drone drone2 = new Drone("D5", "Mini", 5);
+        boolean register1 = rescueCenter.addDrone(drone1);
+        boolean register2 = rescueCenter.addDrone(drone2);
+        assertTrue(register1);
+        assertFalse(register2);
+    }
     
+    @Test 
+    void shouldNotAssignMissionWithDistanceExceedingRange(){
+        Drone drone = new Drone("D6", "Small", 4);
+        RescueOperator rOperator = new RescueOperator("RO", "Daniel");
+        assertThrows(IllegalArgumentException.class,() -> {
+            rescueCenter.addDrone(drone);
+            rescueCenter.addOperator(rOperator);
+            rescueCenter.assignMission("RO", "D6", "Medellin", 10);
+        });
+    }
+
+    @Test
+    void shouldCloseMissionWithoutModifyingActiveOne(){
+        Drone drone = new Drone("D6", "Small", 4);
+        RescueOperator rOperator = new RescueOperator("RO", "Daniel");
+        rescueCenter.addDrone(drone);
+        rescueCenter.addOperator(rOperator);
+        Mission mission1 = rescueCenter.assignMission("RO", "D6", "Medellin", 2);
+
+        Drone drone2 = new Drone("D7", "Medium", 10);
+        RescueOperator rOperator2 = new RescueOperator("RO2", "Paco");
+        rescueCenter.addDrone(drone2);
+        rescueCenter.addOperator(rOperator2);
+        Mission mission2 = rescueCenter.assignMission("RO2", "D7", "Cali", 5);
+
+        assertEquals(MissionStatus.ACTIVE, mission1.getStatus());
+        assertEquals(MissionStatus.ACTIVE, mission2.getStatus());
+
+        rescueCenter.completeMission(mission2.getId());
+
+        assertEquals(MissionStatus.COMPLETED, mission2.getStatus());
+        assertEquals(MissionStatus.ACTIVE, mission1.getStatus());
+        assertTrue(drone.isAvailable());
+    }
 
 }
